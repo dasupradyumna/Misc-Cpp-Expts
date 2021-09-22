8692bf1c43113d59e7ba4300af5db3815f8ed34e
pradyudasu <pradyu1104@gmail.com>
2021-09-22 13:55:59 +0530

    Implemented iterative merge sort

--- sort.cpp
+++ sort.cpp
@@ -2,10 +2,10 @@
 
 #include <algorithm>      // sort
 #include <array>
-#include <iostream>       // cin, cout
-#include <iterator>       // iterator_traits
 #include <ctime>          // time
-#include <vector>
+#include <iostream>       // cin, cout
+#include <iterator>       // iterator_traits, distance
+#include <utility>        // pair
 
 sort::sort( SortType type ) :
   __type { type }
@@ -25,19 +25,19 @@ template<typename _Iter, typename _Pred>
 void sort::__bubble( _Iter begin, _Iter end, _Pred pred )
 {
   using diff_t = typename std::iterator_traits<_Iter>::difference_type;
-  diff_t n_iters { end - begin - 1 };
-  for ( diff_t i { -1 }; ++i < n_iters; )
+  diff_t nIters { std::distance( begin, end ) - 1 };
+  for ( diff_t i { -1 }; ++i < nIters; )
   {
-    bool noswap { true };
+    bool noSwaps { true };
     _Iter _end { end - i };
     for ( _Iter j { begin }; ++j != _end; )
       if ( pred( *j, *(j - 1) ) )
       {
         std::iter_swap( j - 1, j );
-        noswap = false;
+        noSwaps = false;
       }
 
-    if ( noswap ) break;
+    if ( noSwaps ) break;
   }
 }
 
@@ -58,10 +58,9 @@ void sort::__selection( _Iter begin, _Iter end, _Pred pred )
 template<typename _Iter, typename _Pred>
 void sort::__insertion( _Iter begin, _Iter end, _Pred pred )
 {
-  for ( _Iter i { begin }; ++i != end; )
+  for ( _Iter current { begin }; ++current != end; )
   {
-    _Iter key { i };
-    _Iter j { i };
+    _Iter j { current };
     while ( j != begin && pred( *j, *(j - 1) ) )
     {
       std::iter_swap( j, j - 1 );
@@ -71,23 +70,69 @@ void sort::__insertion( _Iter begin, _Iter end, _Pred pred )
 }
 
 template<typename _Iter, typename _Pred>
+void sort::__merge( _Iter begin, _Iter end, _Pred pred )
+{
+  using diff_t = typename std::iterator_traits<_Iter>::difference_type;
+  using value_t = typename std::iterator_traits<_Iter>::value_type;
+  using buf_iter = typename std::vector<value_t>::iterator;
+
+  diff_t conSize { std::distance( begin, end ) };
+  std::vector<value_t> buffer( conSize );
+  for ( int mergeSize { 2 }; mergeSize >> 1 < conSize; mergeSize <<= 1 )
+  {
+    _Iter subCon { begin };
+    buf_iter subBuf { buffer.begin() };
+    for ( ; ; subCon += mergeSize, subBuf += mergeSize )
+    {
+      bool isLastMerge { std::distance( subCon, end ) <= mergeSize };
+
+      _Iter merger { subCon };
+      const _Iter mergeEnd { isLastMerge ? end : subCon + mergeSize };
+      std::copy( subCon, mergeEnd, subBuf );
+
+      buf_iter first { subBuf };
+      const buf_iter bufMid {
+        (std::distance( subBuf, buffer.end() ) <= mergeSize >> 1)
+        ? buffer.end()
+        : subBuf + mergeSize >> 1
+      };
+      buf_iter second { bufMid };
+      const buf_iter bufEnd { isLastMerge ? buffer.end() : subBuf + mergeSize };
+
+      for ( ; merger != mergeEnd; ++merger )
+        if ( first != bufMid && second != bufEnd )
+          std::iter_swap( merger, pred( *first, *second ) ? first++ : second++ );
+        else if ( first != bufMid )
+          std::iter_swap( merger, first++ );
+        else if ( second != bufEnd )
+          std::iter_swap( merger, second++ );
+
+      if ( isLastMerge ) break;
+    }
+  }
+}
+
+template<typename _Iter, typename _Pred>
 void sort::operator()( _Iter begin, _Iter end, _Pred pred )
 {
-  if ( begin != end )
-    switch ( __type )
-    {
-      case SortType::Bubble: __bubble( begin, end, pred ); break;
-      case SortType::Selection: __selection( begin, end, pred ); break;
-      case SortType::Insertion: __insertion( begin, end, pred ); break;
-      case SortType::STD: std::sort( begin, end, pred ); break;
-    }
+  if ( begin == end || begin + 1 == end )
+    return;
+
+  switch ( __type )
+  {
+    case SortType::Bubble: __bubble( begin, end, pred ); break;
+    case SortType::Selection: __selection( begin, end, pred ); break;
+    case SortType::Insertion: __insertion( begin, end, pred ); break;
+    case SortType::Merge: __merge( begin, end, pred ); break;
+    case SortType::STD: std::sort( begin, end, pred ); break;
+  }
 }
 
 void testSort()
 {
   srand( static_cast<unsigned int>(time( nullptr )) );
 
-  constexpr size_t N { 50 };
+  constexpr size_t N { 37 };
   //int A[N] { };
   //for ( int i { -1 }; ++i < N; )
   //  A[i] = rand() % N;
@@ -96,8 +141,11 @@ void testSort()
   for ( auto& el : A )
     el = rand() % N;
   std::cout << std::boolalpha << sort::check( A.begin(), A.end() ) << '\n';
+  for ( const auto& el : A )
+    std::cout << el << ' ';
+  std::cout << "\n\n";
 
-  sort sorter { SortType::Selection };
+  sort sorter { SortType::Merge };
   sorter( A.begin(), A.end() );
   std::cout << std::boolalpha << sort::check( A.begin(), A.end() ) << '\n';
 
